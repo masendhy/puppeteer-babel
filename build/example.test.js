b46e5393388d76f1641eb60349a54a0c4993c886
heejra <theheejra@gmail.com>
2020-12-11 16:46:39 +0700

    mocha steps done

--- build/example.test.js
+++ build/example.test.js
@@ -8,19 +8,42 @@ var _homepage = require("./homepage");
 
 var _homepage2 = _interopRequireDefault(_homepage);
 
+var _mochaSteps = require("mocha-steps");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// const puppeteer = require("puppeteer")
-describe("load the URL", function () {
-    it("should work", async function () {
-        var browser = await _puppeteer2.default.launch({
-            headless: false
-        });
-        var page = await browser.newPage();
+describe("Mocha steps test", function () {
+    var browser = void 0,
+        page = void 0;
+
+    before(async function () {
+        browser = await _puppeteer2.default.launch({ headless: false });
+        page = await browser.newPage();
+        await page.setDefaultTimeout(7000);
+    });
+
+    after(async function () {
+        await browser.close();
+    });
+
+    (0, _mochaSteps.step)("should load homepage", async function () {
+        // const browser = await puppeteer.launch({
+        //     headless: false
+        // });
+        // const page = await browser.newPage();
         var homepage = new _homepage2.default(page);
+
         // await page.goto("http://zero.webappsecurity.com/index.html");
         // await page.waitFor(5000);
         await homepage.visit();
-        await browser.close();
+        // await browser.close();
     });
-});
+
+    (0, _mochaSteps.step)("step 2", async function () {
+        await page.waitForSelector("#FAIL");
+    });
+
+    (0, _mochaSteps.step)("step e", async function () {
+        await page.waitForSelector("#FAIL");
+    });
+}); // const puppeteer = require("puppeteer")
--- tests/example.test.js
+++ tests/example.test.js
@@ -1,18 +1,40 @@
 // const puppeteer = require("puppeteer")
 import puppeteer from "puppeteer";
 import Homepage from "./homepage";
+import { step, steps } from "mocha-steps";
 
 
-describe("load the URL", () => {
-    it("should work", async () => {
-        const browser = await puppeteer.launch({
-            headless: false
-        });
-        const page = await browser.newPage();
+describe("Mocha steps test", () => {
+let browser, page ;
+
+before (async () =>{
+    browser = await puppeteer.launch({ headless : false});
+    page = await browser.newPage();
+    await page.setDefaultTimeout(7000);
+});
+
+after (async () => {
+    await browser.close();
+});
+
+    step("should load homepage", async () => {
+        // const browser = await puppeteer.launch({
+        //     headless: false
+        // });
+        // const page = await browser.newPage();
         const homepage = new Homepage(page);
+
         // await page.goto("http://zero.webappsecurity.com/index.html");
         // await page.waitFor(5000);
         await homepage.visit();
-        await browser.close();
+        // await browser.close();
     });
+
+    step("step 2", async()=> {
+        await page.waitForSelector("#FAIL");
+    });
+
+    step("step e", async ()=>{
+        await page.waitForSelector("#FAIL");
+});
 });
